7d3bbaafc5d3f55bcbe95077fb89a7ed3fa3a901
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-04-10 14:57:41 +0200

    fix: added major version of distribution centos and rhel with latest build of upstream image

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ locals {
   os_version = "${element(split("_", var.os),1)}"
 
   os_special_version_script = {
-    centos = ["7.3", "7.5"]
+    centos = ["7.3", "7.5", "7"]
     coreos = []
     rhel   = []
   }
--- variables.tf
+++ variables.tf
@@ -16,16 +16,20 @@ variable "os_image_version" {
   description = "GCP Images"
   type        = "map"
 
-  # GCP Image Schema # image family   /   image name
+  # GCP Image Schema # Image project  /   image name
   #                                  V                  V
+  # mayor version only should match latest updated from upstream and stable
   default = {
     "centos_7.2"      = ["centos-cloud", "centos-7-v20170523"]
     "centos_7.3"      = ["centos-cloud", "centos-7-v20170719"]
-    "centos_7.5"      = ["centos-cloud", "centos-7-v20181011", "centos-7-v20190326"]
+    "centos_7.5"      = ["centos-cloud", "centos-7-v20181011"]
+    "centos_7"        = ["centos-cloud", "centos-7-v20190326"]
     "coreos_stable"   = ["coreos-cloud", "coreos-stable"]
     "coreos_1576.5.0" = ["coreos-cloud", "coreos-stable-1576-5-0-v20180105"]
     "coreos_1855.5.0" = ["coreos-cloud", "coreos-stable-1855-5-0-v20181024"]
+    "coreos_2023.5.0" = ["coreos-cloud", "coreos-stable-2023-5-0-v20190312"]
     "rhel_7.3"        = ["rhel-cloud", "rhel-7-v20170523"]
+    "rhel_7"          = ["rhel-cloud", "rhel-7-v20190326"]
     ""                = ["", ""]
   }
 }
