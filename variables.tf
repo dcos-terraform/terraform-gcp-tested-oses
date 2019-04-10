927fc78616c27453c7bde9aebef360dd1a916dac
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-04-10 15:16:43 +0200

    chore: added the image family as image name instead to have always the latest stable / available non deprecated

--- variables.tf
+++ variables.tf
@@ -23,13 +23,13 @@ variable "os_image_version" {
     "centos_7.2"      = ["centos-cloud", "centos-7-v20170523"]
     "centos_7.3"      = ["centos-cloud", "centos-7-v20170719"]
     "centos_7.5"      = ["centos-cloud", "centos-7-v20181011"]
-    "centos_7"        = ["centos-cloud", "centos-7-v20190326"]
+    "centos_7"        = ["centos-cloud", "centos-7"]
     "coreos_stable"   = ["coreos-cloud", "coreos-stable"]
     "coreos_1576.5.0" = ["coreos-cloud", "coreos-stable-1576-5-0-v20180105"]
     "coreos_1855.5.0" = ["coreos-cloud", "coreos-stable-1855-5-0-v20181024"]
     "coreos_2023.5.0" = ["coreos-cloud", "coreos-stable-2023-5-0-v20190312"]
     "rhel_7.3"        = ["rhel-cloud", "rhel-7-v20170523"]
-    "rhel_7"          = ["rhel-cloud", "rhel-7-v20190326"]
+    "rhel_7"          = ["rhel-cloud", "rhel-7"]
     ""                = ["", ""]
   }
 }
