88038ccd84177b589c047a7cecda5e20ef97e02b
Jan Ulferts <julferts@mesosphere.com>
2020-06-08 14:45:05 +0200

    feat: initial tf 0.12 port (#6)

--- main.tf
+++ main.tf
@@ -16,17 +16,10 @@
  */
 
 locals {
-  os_name    = "${element(split("_", var.os),0)}"
-  os_version = "${element(split("_", var.os),1)}"
+  os_name    = element(split("_", var.os), 0)
+  os_version = element(split("_", var.os), 1)
 
-  os_special_version_script = {
-    centos = ["7.3", "7.5", "7"]
-    coreos = []
-    rhel   = []
-  }
-
-  user         = "${lookup(var.traditional_default_os_user, local.os_name)}"
-  image_family = "${element(var.os_image_version[var.os], 0)}"
-  image_name   = "${element(var.os_image_version[var.os], 1)}"
-  script       = "${file("${path.module}/scripts/${local.os_name}/${contains(local.os_special_version_script[local.os_name], local.os_version) ? local.os_version : "setup"}.sh")}"
+  user         = var.traditional_default_os_user[local.os_name]
+  image_family = element(var.os_image_version[var.os], 0)
+  image_name   = element(var.os_image_version[var.os], 1)
 }
